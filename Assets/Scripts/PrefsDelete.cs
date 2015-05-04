07486d5614d5f19112f420247401073bf019f7b2
Dawnfire <jonduke90@gmail.com>
2015-05-04 10:39:36 -0700

    Added a function to clear player preferences from the editor

---
+++ Assets/Scripts/PrefsDelete.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class PrefsDelete : MonoBehaviour {
+
+#if UNITY_EDITOR
+    [UnityEditor.CustomEditor(typeof(PrefsDelete))]
+    public class SnapToGroundEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            PrefsDelete script = target as PrefsDelete;
+            if (GUILayout.Button("CLEAR PLAYER PREFS"))
+                PlayerPrefs.DeleteAll();
+        }
+    }
+#endif
+	
+}
