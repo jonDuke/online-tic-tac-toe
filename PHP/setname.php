2f39c142ff98b037cf98c8c1e8e70ca0323ab6dc
Dawnfire <jonduke90@gmail.com>
2015-05-06 15:08:10 -0700

    setname.php now escapes the name entered (should have done that in the first place)

--- PHP/setname.php
+++ PHP/setname.php
@@ -5,7 +5,7 @@
 	$playerid = $_POST['id'];
 	$newName = $_POST['name'];
 
-	$query = 'UPDATE Players SET name=\'' . $newName . '\' WHERE playerid = ' . $playerid;
+	$query = 'UPDATE Players SET name=\'' . mysqli_escape_string($DB_link, $newName) . '\' WHERE playerid = ' . $playerid;
 	$result = mysqli_query($DB_link, $query) or die('ERROR: ' . mysqli_error($DB_link));
 
 	echo true;
