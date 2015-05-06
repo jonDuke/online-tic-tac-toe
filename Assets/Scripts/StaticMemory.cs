2747e339e88abed783c29e1e85f241f41a9d09ff
Dawnfire <jonduke90@gmail.com>
2015-05-06 15:07:22 -0700

    created a static memory class to pass variables between scenes

---
+++ Assets/Scripts/StaticMemory.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class StaticMemory : MonoBehaviour {
+
+	/*
+     * container class for various static variables I may need to pass between scenes
+     * */
+
+    public static int currentGame; //gameid of the game selected, so we can load the correct one
+}
