bef7e19a2fe4cf36b1c2eaf10afa279fca896d4f
Dawnfire <jonduke90@gmail.com>
2015-05-13 17:33:00 -0700

    Accepting challenges now works and creates a new game

--- Assets/Scripts/Challenge Scripts/ChallengeDisplay.cs
+++ Assets/Scripts/Challenge Scripts/ChallengeDisplay.cs
@@ -1,14 +1,51 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class ChallengeDisplay : MonoBehaviour {
 
     public Text nameDisplay, statDisplay;
     public int playerid;
 
+    private bool clicked = false;
+
+    private string url = "http://noblehousegames.x10host.com/tictactoe/creategame.php";
+
     public void SetDisplay(string name, int wins, int losses, int draws)
     {
         nameDisplay.text = "Opponent: " + name;
         statDisplay.text = "Stats: " + wins + "/" + losses + "/" + draws;
+    }
+
+    public void CreateGame()
+    {
+        if (clicked)
+            return;
+
+        clicked = true; //prevents sending the request multiple times
+
+        WWWForm form = new WWWForm();
+        form.AddField("id", PlayerPrefs.GetInt("playerid"));
+        form.AddField("otherid", playerid);
+
+        WWW www = new WWW(url, form);
+        StartCoroutine(callCreateGame(www));
+    }
+
+    IEnumerator callCreateGame(WWW www)
+    {
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log(www.text);
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+            clicked = false;
+        }
     }
 }
---
+++ PHP/challenge scripts/creategame.php
@@ -0,0 +1,46 @@
+<?php
+	require_once 'app_config.php';
+	$DB_link = connect();
+
+	$playerID = $_POST['id'];
+	$otherID = $_POST['otherid'];
+	
+	$turn = rand(1, 2);
+
+	//create the game
+	$query = "INSERT INTO Games (player1id, player2id, turn) VALUES ($playerID, $otherID, $turn)";
+	$result = mysqli_query($DB_link, $query) or die('ERROR1: ' . mysqli_error($DB_link));
+	$gameID = mysqli_insert_id($DB_link);
+
+	//update player profiles
+	update_profile($playerID, $gameID, $DB_link);
+	update_profile($otherID, $gameID, $DB_link);
+
+	echo json_encode(array('newgame' => $gameID));
+
+
+	function update_profile($id, $newGameID, $DB_link)
+	{
+		$query = "SELECT gameids FROM Players WHERE playerid = $id";
+		$result = mysqli_query($DB_link, $query) or die('ERROR updating: ' . mysqli_error($DB_link));
+
+		if (mysqli_num_rows($result) == 0)
+			die('ERROR: player not found');
+
+		$result = mysqli_fetch_assoc($result);
+		$gameids = json_decode($result['gameids']);
+
+		for($i = 0; $i<10; $i++)
+		{
+			if($gameids[$i] == -1)
+			{
+				$gameids[$i] = (int)$newGameID;
+				break;
+			}
+		}
+
+		$gameids = json_encode($gameids);
+		$query = "UPDATE Players SET gameids = \"$gameids\" WHERE playerid = $id";
+		mysqli_query($DB_link, $query) or die('ERROR updating: ' . mysqli_error($DB_link));
+	}
+?>
