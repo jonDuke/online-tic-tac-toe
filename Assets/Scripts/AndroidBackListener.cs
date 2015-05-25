fb2daad2917b90830ec9e71297b7ceb9e6c9d794
Dawnfire <jonduke90@gmail.com>
2015-05-25 00:51:38 -0700

    Pressing the android back button now takes you to the previous menu

---
+++ Assets/Scripts/AndroidBackListener.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class AndroidBackListener : MonoBehaviour {
+
+    public string sceneToLoad;
+
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (sceneToLoad == "Exit")
+                Application.Quit();
+            else
+                Application.LoadLevel(sceneToLoad);
+        }
+	}
+}
